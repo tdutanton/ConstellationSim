b5d737fda77dfce2cf8c4adccbb9387f2632316a
tdutanton <red1house1@gmail.com>
2026-02-28 17:50:31 +0300

    feat: add central service

--- src/main/java/constellation/Service/SpaceOperationCenterService/SpaceOperationCenterService.java
+++ src/main/java/constellation/Service/SpaceOperationCenterService/SpaceOperationCenterService.java
@@ -2,4 +2,9 @@ package constellation.Service.SpaceOperationCenterService;
 
 public class SpaceOperationCenterService {
 
+  // AddSatelliteRequest – класс, содержащий параметры создаваемой группировки и спутников в ней
+  public void addSatellite(AddSatelliteRequest);
+
+  // класс с параметрами, описывающий какие миссии и на каких спутниках/группировках должны быть выполнены (например, группировка дистанционного зондирования Земли или группировка связи)
+  public void executeMission(MissionRequest);
 }
