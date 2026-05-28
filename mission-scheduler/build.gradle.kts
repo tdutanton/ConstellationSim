80f59f110931d6b38316e80bb790ca4c54c004b1
master <red1house1@gmail.com>
2026-05-28 07:08:30 +0000

    fix: update spring boot version

--- mission-scheduler/build.gradle.kts
+++ mission-scheduler/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.6"
+    id("org.springframework.boot") version "3.3.11"
     id("io.spring.dependency-management") version "1.1.7"
     id("jacoco")
 }
